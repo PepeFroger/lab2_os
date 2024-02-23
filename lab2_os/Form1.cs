e4a33ac137ec2e1ac5a54276e2e3482f10f72c2b
Aleksandr Petrov <vigasurt@gmail.com>
2024-02-23 14:05:38 +0300

    lab2_os

---
+++ lab2_os/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab2_os
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                try
+                {
+                    label1.Text += "\n" + drive.Name + "\n" + Convert.ToString(drive.DriveType)
+                        + "\n" + Convert.ToString(drive.TotalSize) + "\n" + Convert.ToString(drive.AvailableFreeSpace);
+                    
+
+                }
+                catch { }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = null;
+        }
+    }
+}
---
+++ lab2_os/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2_os
+{
+    internal static class Program
+    {
+        private static void DATA_disk(string[] args) 
+        {
+        
+        }
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
