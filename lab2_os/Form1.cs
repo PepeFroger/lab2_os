65a127b731a8d24ff88e16f4eeda164f67046cee
Aleksandr Petrov <vigasurt@gmail.com>
2024-02-23 14:07:37 +0300

    lab2_os_2

--- lab2_os/Form1.cs
+++ lab2_os/Form1.cs
@@ -41,17 +41,6 @@ namespace lab2_os
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
